0bfead0eb7da38d39a19170acdc492e78f5682d5
Arteeex <arteeex187@gmail.com>
2023-01-19 13:32:24 +0700

    Zadacha_13

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (a < 1000)
+{
+    int result_1 = a %10;
+    Console.WriteLine(result_1);   
+}
+else if (a > 9999)
+{
+    int result_2 = (a % 11);
+    Console.WriteLine(result_2);  
+}
+else
+{
+int result = (a / 10) % 10;
+Console.WriteLine(result);
+}
