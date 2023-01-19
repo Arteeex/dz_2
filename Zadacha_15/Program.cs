946644678dd087b5f1c049c460cc2961136c0492
Arteeex <arteeex187@gmail.com>
2023-01-19 13:39:21 +0700

    Zadacha_15

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер дня недели: ");
+string a = Console.ReadLine();
+switch(a)
+{
+    case "1":
+    Console.WriteLine("Понедельник, рабочий день");
+    break;
+    
+    case "2":
+    Console.WriteLine("Вторник, рабочий день");
+    break;
+
+    case "3":
+    Console.WriteLine("Среда, рабочий день");
+    break;
+
+    case "4":
+    Console.WriteLine("Четверг, рабочий день");
+    break;
+
+    case "5":
+    Console.WriteLine("Пятница, рабочий день");
+    break;
+
+    case "6":
+    Console.WriteLine("Суббота, выходной день");
+    break;
+
+    case "7":
+    Console.WriteLine("Воскресенье, выходной день");
+    break;
+}
